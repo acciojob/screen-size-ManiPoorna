5354aefc22e665e09cf7ab965f9736fd1646ceff
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-05 20:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const w = document.getElementById("width")
+const h = document.getElementById("height")
+
+window.addEventListener("resize",(e)=>{
+	e.preventDefault();
+	w.innerText = window.innerWidth
+	h.innerText = window.innerHeight
+})
